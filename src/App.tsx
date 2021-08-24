246fa49e8f3f8a91d5e1fc9785e33d8577c7730b
dodytrifa <dody.trifa@gmail.com>
2021-08-24 22:02:24 +0700

    improve initialization quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import React, {useState} from 'react';
 import QuestionCard from './components/QuestionCard'
-import { fetchQuest, Difficulty } from './API';
-
+import { QuestionState, fetchQuest, Difficulty } from './API';
 
 const TOTAL_QUESTIONS = 10
 
-function App() {
-  const [loading,setLoading] = useState(false)  
-  const [questions, setQuestions] = useState([])
-  const [number, setNumber] = useState(0)
-  const [userAnswers, setUserAnswers] = useState([])
-  const [score, setScore] = useState(0)
-  const [gameOver, setGameOver] = useState(false)
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string
+}
 
-  console.log(fetchQuest(TOTAL_QUESTIONS,Difficulty.EASY));
+const App: React.FC = () => {
+  const [loading,setLoading] = useState(false)  
+  const [questions, setQuestions] = useState<QuestionState[]>([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+ 
+  // console.log(fetchQuest(TOTAL_QUESTIONS,Difficulty.       EASY));
   
 
   const initQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuest(TOTAL_QUESTIONS, Difficulty.EASY);
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false)
   }
 
   const answer = (e: React.MouseEvent<HTMLButtonElement>) => {
 
   }
 
+  const initQuizz = () => {
+    console.log("halo");
+    
+  }
   const nextQuestion = () => {
 
   }
@@ -31,9 +50,17 @@ function App() {
   return (
     <div className="App">
       <h1>A QUIZ BUILD WITH REACT TYPESCRIPT</h1>
-      <button className='start' onClick={initQuiz}>Start</button>
-      <p className='score'>Score:</p>
-      <p>Loading Questions...</p>
+      
+      {/* <button className='start' onClick={initQuizz}>Start</button> */}
+      {
+      gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+      <button className='start' onClick={initQuiz}>
+          Start
+          </button>
+      ): null 
+      }
+      {!gameOver ? <p className='score'>Score:</p> : null}
+      {loading && <p>Loading Questions...</p>}
       {/* <QuestionCard
       questionNumber={number + 1}
       totalQuestion={TOTAL_QUESTIONS}
